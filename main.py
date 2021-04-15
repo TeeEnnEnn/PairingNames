e115360ade21a2257906b1ef46336232bd817145
TeeEnnEnn <theonenhanga@gmail.com>
2021-04-15 16:52:40 +0200

    Completion.
    The list of paired names can now be saved to a .dat file. The .dat file can read to ensure that the pairs are not the same as a previous list.
    
    I have also commented my code to improve readability.

--- main.py
+++ main.py
@@ -8,12 +8,30 @@ import os
 from datetime import date
 
 
+def open_dat():
+    """This function opens a .dat file and reads the saved list from it."""
+    # Current Working Directory
+    cwd = os.getcwd()
+    try:
+        previous_list = pickle.load(open(cwd + "\\savedList.dat", "rb"))
+        return previous_list
+    except FileNotFoundError:
+        return []
+
+def validate_pair(pair_to_validate):
+    """This function checks if the pair has been used in the previous list of pairs."""
+    previous: list = open_dat()
+    if previous:
+        # dim_1: dimension_1
+        for dim_1 in previous:
+            if pair_to_validate in dim_1 or pair_to_validate.reverse() in dim_1:
+                return False
+        return True
+    else:
+        return True
+
 def get_pairs():
     """This function is used to get the list of names that will be used for pairing."""
-
-    def validate_pair(pair_to_validate):
-        """This function checks if the pair has been used in the previous list of pairs."""
-        pass
 
     print("How many pairs would you want to make?\n")
 
@@ -53,9 +71,14 @@ def get_pairs():
 
 
 def randomize_names(names_list):
+    """This function is used to create the random pairs."""
     randomized = []
     for x in range(int(len(names_list)/2)):
         pair = random.sample(names_list, 2)
+        if validate_pair(pair):
+            pass
+        else:
+            continue
         
         duo = pair.copy()
         print(f"Pair:   {pair}\n")
@@ -66,12 +89,13 @@ def randomize_names(names_list):
         names_list.remove(first)
         names_list.remove(second)
 
-    print("This is the randomized list.")
+    print("This is the randomized list:")
     print(randomized)
     return randomized
 
 
 def make_text(list_to_text):
+    """This function saves the list as a text file"""
     today = date.today()
 
     try:
@@ -86,14 +110,31 @@ def make_text(list_to_text):
 
 
 def save_to_dat(list_to_be_saved):
-    """This function creates"""
+    """This function creates a dat file and saves the list of names."""
     # Current Working Directory
     cwd = os.getcwd()
-    pickle.dump(list_to_be_saved, open("savedList.dat", "wb"))
+    pickle.dump(list_to_be_saved, open(cwd + "\\savedList.dat", "wb"))
+    print("The list has been saved to: savedList.dat\n")
 
 
 def menu():
-    """This function will have the menu that is seen when the program is started."""
+    """This function has the functions that are used when running the program."""
     variable = get_pairs()
     names = randomize_names(variable)
     make_text(names)
+    save_to_dat(names)
+
+# This section runs the code.
+if __name__ == "__main__":
+    while True:
+        print("Welcome to the Pairing System\n")
+        menu()
+        print("Thank you for using the pairing system.\n")
+        print("Would you like to\n1.)Use the paring system again\n2.)Exit")
+        choice = input()
+        if choice.lower() in ["1", "1.", "1.)", "one"]:
+            continue
+        else:
+            break
+    print("\nThank you for using The Pairing System\n")
+    input()
